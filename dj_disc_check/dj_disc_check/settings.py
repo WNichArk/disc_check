85d6fb640db80e72a9857ed011e5cd3576842b19
Will Nicholson <william.nicholson.dev@gmail.com>
2022-07-18 09:57:34 -0500

    modify django keys; adds project constants

--- dj_disc_check/dj_disc_check/settings.py
+++ dj_disc_check/dj_disc_check/settings.py
@@ -10,8 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import constants.constants as cs
 from pathlib import Path
-from constants.constants import DJANGO_SECRET_KEY as DSK
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = DSK
+SECRET_KEY = cs.DJANGO_SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
